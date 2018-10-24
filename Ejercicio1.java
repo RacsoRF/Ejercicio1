b3ed3abf692fa03463c5def5b0c3a7f5c3a2e4c8
RacsoRF <oscar_adrian9@hotmail.com>
2018-10-24 17:48:03 -0600

    Primer commit

---
+++ Ejercicio1.java
@@ -0,0 +1,22 @@
+
+public class Ejercicio1 {
+
+	public static void main (String args[]){
+
+		int N1,N2;
+		N1= 200;
+		N2=400;
+
+System.out.println("La sumatoria es " + ( N1+N2));
+
+System.out.println("La resta es " + ( N1-N2));
+
+System.out.println("La divison es " + ( N1/N2));
+
+System.out.println("La multiplicacion es " + ( N1*N2));
+
+System.out.println("El modulo es " + ( N1%N2));
+
+
+	}
+}
